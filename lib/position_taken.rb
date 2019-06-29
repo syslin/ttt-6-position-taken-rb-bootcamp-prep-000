8f31ceb964bf7d7eeac7b9b7e1e0219367cc0c8a
Sanima Shrestha <saneemastha@gmail.com>
2019-06-29 14:27:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,3 +6,12 @@ def position_taken?(board, index)
   
 end
 board=["","","","","","","","",""]
+
+
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index]=="O"
+    true
+ 
+  end
